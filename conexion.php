c70f07a58ea6a8a657e215d5673d47d908bc7dac
AlejandroGasca09 <119777270+AlejandroGasca09@users.noreply.github.com>
2024-05-20 23:54:50 -0600

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php
-$user_db="if0_36150764";
-$pass_db="W6UVaR3PUL7Gi";
-$db_name="if0_36150764_ICO";
-$host_db="sql101.infinityfree.com";
+$user_db="root";
+$pass_db="";
+$db_name="local";
+$host_db="localhost:127.0.0.1";
 $conexion = new mysqli($host_db,$user_db,$pass_db,$db_name);
 if($conexion-> connect_error){
     die("La conexion no sirve ".$conexion->connect_error);
